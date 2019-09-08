c3d18833be39aa316425f8d3f712ed575a052b2a
Ivan Soloducha <ivan@soloduha.com>
2019-09-08 21:00:40 +0300

    Initial commit -- Actix app.

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use actix_web::{http, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
+use futures::future::{ok, Future};
+
+
+fn index(_req: HttpRequest) -> Box<dyn Future<Item = HttpResponse, Error = Error>> {
+    Box::new(ok::<_, Error>(
+        HttpResponse::Ok().content_type("text/html").body("Hello!"),
+    ))
+}
+
+fn main() {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+    })
+    .bind("127.0.0.1:8088")
+    .unwrap()
+    .run()
+    .unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::test;
+
+    #[test]
+    fn test_index_ok() {
+        let req = test::TestRequest::with_header("content-type", "multipart/form-data").to_http_request();
+
+        let resp = test::block_on(index(req)).unwrap();
+        assert_eq!(resp.status(), http::StatusCode::OK)
+    }
+}
